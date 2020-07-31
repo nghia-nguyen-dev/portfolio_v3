087a2c93c82513369317cccd581fa624fd94a6ad
nghia-nguyen-dev <60081184+nghia-nguyen-dev@users.noreply.github.com>
2020-07-31 17:45:03 -0400

    .

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const scrollGrp = document.querySelector('.scroll-grp');
 const header = document.querySelector('header');
+const controls = document.querySelector('.controls');
 
 const projects = [...document.querySelectorAll('.work')]
 
@@ -9,12 +10,21 @@ let counter = '';
 // ------------------------ EVENT LISTENERS ------------------------
 
 scrollGrp.addEventListener('click', function(e) {
-  
+   
+
+
     if (e.target.classList.contains('loop-container')) {
+        
+
+
         removeCard();
         displayCard(e);
         removeSelectedText();
         selectedTextLine(e);
+
+
+
+        
     }
 
     if (e.target.classList.contains('x-icon')) {
@@ -30,32 +40,70 @@ scrollGrp.addEventListener('click', function(e) {
 
 })
 
+controls.addEventListener('click', function(e) {
+  
+    if (e.target.className === 'arrow') {
+        if (e.target.dataset.arrow === 'left') {
+            prev();
+        } else {
+            next();
+        }
+    }
+
+})
+
+
 
 // ------------------------ FUNCTIONS ------------------------
+
+function prev() {
+    if (counter === 0) {
+        return
+    } else {
+        removeProject();
+        counter = counter - 1;
+        projects[counter].style.display = 'block';
+    }
+}
+
+function next() {
+    if (counter === projects.length - 1) {
+        console.log('object');
+        return
+    } else {
+        removeProject();
+        counter = counter + 1;
+        projects[counter].style.display = 'block';
+    }
+}
+
+
+
+
 
 function displayMainAndLine() {
     const main = document.querySelector('main');
     const line = document.querySelector('.line');
+    
     main.style.display = 'block';
     line.style.display = 'block';
+    controls.style.display = 'flex'
 }
 
 function removeProject() {
-
     projects.forEach(proj => {
         proj.style.display = '';
     })
 }
 
 function displayProject(e) {
-    const currentProject = parseInt(e.target.dataset.project) - 1;
+    const currentProject = parseInt(e.target.dataset.project);
     counter = currentProject;
     projects[currentProject].style.display = 'block';
 }
 
 
 function selectedTextLine (e) {
-
     const loopContainerTxts = [...e.target.children];
     loopContainerTxts.forEach(each => {
         each.classList.add('selected-txt');
@@ -65,15 +113,26 @@ function selectedTextLine (e) {
 
 function displayCard(e) {
 
+    
+
+    console.log(e.target.lastElementChild.offsetWidth);
+
     const activeCard = e.target.lastElementChild;
     activeCard.classList.add('card-active')
 
-    const mouseX = e.clientX;
+    let mouseX = e.clientX;
     const mouseY = e.clientY;
+
+    // safeguard for info card from appearing off frame if clicked close to left edge
+    if (mouseX > window.innerWidth - e.target.lastElementChild.offsetWidth) {
+        mouseX = (window.innerWidth - e.target.lastElementChild.offsetWidth) - 20;
+    }
 
     activeCard.style.left = `${mouseX}px`;
     activeCard.style.top = `${mouseY}px`;
 
+
+   
 }
 
 function removeSelectedText() {
@@ -104,3 +163,20 @@ function removeCard() {
 // display main
 // display vertical line
 // display chosen project
+
+function getCurrentTime() {
+    const today = new Date();
+    const hour = (today.getHours() - 12) * -1;
+    const minute = today.getMinutes();
+    const timeOfDay = getPmOrAm();
+}
+
+function getPmOrAm () {
+    const today = new Date();
+    const hour = today.getHours();
+    if (hour >= 12) {
+        return 'PM'
+    } else {
+        return 'AM'
+    }
+}
